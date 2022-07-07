77d65b04a53a161c91d264a8ad4e73b5eeaec558
Romi Hervier <r@sansfontieres.com>
2022-07-07 13:55:32 +0200

    Formatting

--- prompt.pas
+++ prompt.pas
@@ -9,49 +9,49 @@ var code: integer = 0;
 
 function local_session(): boolean;
 begin
-	local_session := length(getEnvironmentVariable('SSH_CONNECTION')) = 0;
+    local_session := length(getEnvironmentVariable('SSH_CONNECTION')) = 0;
 end;
 
 function rel_home(homedir, cwd: string): boolean;
 var i: integer;
 begin
-	if length(homedir) <= length(cwd) then
-		i := compareStr(homedir, copy(cwd, 0, length(homedir)));
-	if i = 0 then rel_home := true else rel_home := false;
+    if length(homedir) <= length(cwd) then
+        i := compareStr(homedir, copy(cwd, 0, length(homedir)));
+    if i = 0 then rel_home := true else rel_home := false;
 end;
 
 procedure path_print();
 var
-	homedir: string;
-	path: string;
+    homedir: string;
+    path: string;
 begin
-	homedir := getEnvironmentVariable('HOME');
+    homedir := getEnvironmentVariable('HOME');
 
-	write('%{' + #27 + '[32m%}');
+    write('%{' + #27 + '[32m%}');
 
-	if not local_session() then
-		write(format('%s:', [getHostName]));
+    if not local_session() then
+        write(format('%s:', [getHostName]));
 
-	if rel_home(homedir, getCurrentDir) then
-		path := '~' + copy(getCurrentDir, length(homedir) + 1, length(getCurrentDir))
-	else
-		path := getCurrentDir;
-	
-	write(path + reset_code);
+    if rel_home(homedir, getCurrentDir) then
+        path := '~' + copy(getCurrentDir, length(homedir) + 1, length(getCurrentDir))
+    else
+        path := getCurrentDir;
+
+    write(path + reset_code);
 end;
 
 procedure prompt_print(code: integer);
 begin
-	if code = 0 then
-		write('; ')
-	else
-		write('%{' + #27 + '[31m%};' + reset_code + ' ');
+    if code = 0 then
+        write('; ')
+    else
+        write('%{' + #27 + '[31m%};' + reset_code + ' ');
 end;
 
 begin
-	if argv[1] <> nil then
-		code := strToInt(argv[1]);
+    if argv[1] <> nil then
+        code := strToInt(argv[1]);
 
-	path_print();
-	prompt_print(code);
+    path_print();
+    prompt_print(code);
 end.
