a593613b61eaa6eb739f8efb2850307dffc31d50
Romi Hervier <r@sansfontieres.com>
2022-07-07 13:55:32 +0200

    Added a prompt in Pascal

---
+++ prompt.pas
@@ -0,0 +1,59 @@
+// Just fun throwaway code as my prompt
+program prompt;
+
+uses
+	SysUtils, unix;
+
+var
+	code: integer = 0;
+
+function local_session(): boolean;
+begin
+	local_session := length(getEnvironmentVariable('SSH_CONNECTION')) = 0;
+end;
+
+function rel_home(homedir, cwd: string): boolean;
+var
+	i: integer;
+begin
+	if length(homedir) <= length(cwd) then
+		i := compareStr(homedir, copy(cwd, 0, length(homedir)));
+	if i = 0 then rel_home := true else rel_home := false;
+end;
+
+procedure path_print();
+var
+	homedir: string;
+	path: string;
+begin
+	homedir := getEnvironmentVariable('HOME');
+
+	write('%{' + #27 + '[32m%}');
+
+	if not local_session() then
+		write(stdout, format('%s:', [getHostName]));
+
+	if rel_home(homedir, getCurrentDir) then
+		path := '~' + copy(getCurrentDir, length(homedir)+1, length(getCurrentDir))
+	else
+		path := getCurrentDir;
+	
+	write(path);
+	write('%{' + #27 + '[0m%}');
+end;
+
+procedure prompt_print(code: integer);
+begin
+	if code = 0 then
+		write('; ')
+	else
+		write('%{' + #27 + '[31m%};%{' + #27 + '[0m%} ');
+end;
+
+begin
+	if argv[1] <> nil then
+		code := strToInt(argv[1]);
+
+	path_print();
+	prompt_print(code);
+end.
